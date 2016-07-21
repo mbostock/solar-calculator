c83d5fcd50e67d4ec55a08a96551a50afcb50cba
Mike Bostock <mbostock@gmail.com>
2016-07-21 08:54:24 -0400

    Update to d3-time@1.

--- index.js
+++ index.js
@@ -3,36 +3,45 @@
 
 (function() {
   var J2000 = Date.UTC(2000, 0, 1, 12),
-      π = Math.PI,
-      τ = 2 * π,
-      radians = π / 180,
-      degrees = 180 / π;
+      abs = Math.abs,
+      acos = Math.acos,
+      asin = Math.asin,
+      cos = Math.cos,
+      max = Math.max,
+      min = Math.min,
+      sin = Math.sin,
+      tan = Math.tan,
+      epsilon = 1e-6,
+      pi = Math.PI,
+      tau = 2 * pi,
+      radians = pi / 180,
+      degrees = 180 / pi;
 
   solarCalculator = function(location) {
     var longitude = location[0],
         minutesOffset = 720 - longitude * 4,
-        λ = location[0] * radians,
-        φ = location[1] * radians,
-        cosφ = Math.cos(φ),
-        sinφ = Math.sin(φ);
+        x = location[0] * radians,
+        y = location[1] * radians,
+        cy = cos(y),
+        sy = sin(y);
 
     function position(date) {
       var centuries = (date - J2000) / (864e5 * 36525),
-          θ = solarDeclination(centuries),
-          cosθ = Math.cos(θ),
-          sinθ = Math.sin(θ),
-          azimuth = ((date - d3.time.day.utc.floor(date)) / 864e5 * τ + equationOfTime(centuries) + λ) % τ - π,
-          zenith = Math.acos(Math.max(-1, Math.min(1, sinφ * sinθ + cosφ * cosθ * Math.cos(azimuth)))),
-          azimuthDenominator = cosφ * Math.sin(zenith);
+          theta = solarDeclination(centuries),
+          ctheta = cos(theta),
+          stheta = sin(theta),
+          azimuth = ((date - d3.utcDay.floor(date)) / 864e5 * tau + equationOfTime(centuries) + x) % tau - pi,
+          zenith = acos(max(-1, min(1, sy * stheta + cy * ctheta * cos(azimuth)))),
+          azimuthDenominator = cy * sin(zenith);
 
-      if (azimuth < -π) azimuth += τ;
-      if (Math.abs(azimuthDenominator) > 1e-6) azimuth = (azimuth > 0 ? -1 : 1) * (π - Math.acos(Math.max(-1, Math.min(1, (sinφ * Math.cos(zenith) - sinθ) / azimuthDenominator))));
-      if (azimuth < 0) azimuth += τ;
+      if (azimuth < -pi) azimuth += tau;
+      if (abs(azimuthDenominator) > epsilon) azimuth = (azimuth > 0 ? -1 : 1) * (pi - acos(max(-1, min(1, (sy * cos(zenith) - stheta) / azimuthDenominator))));
+      if (azimuth < 0) azimuth += tau;
 
       // Correct for atmospheric refraction.
       var atmosphere = 90 - zenith * degrees;
       if (atmosphere <= 85) {
-        var te = Math.tan(atmosphere * radians);
+        var te = tan(atmosphere * radians);
         zenith -= (atmosphere > 5 ? 58.1 / te - .07 / (te * te * te) + .000086 / (te * te * te * te * te)
             : atmosphere > -.575 ? 1735 + atmosphere * (-518.2 + atmosphere * (103.4 + atmosphere * (-12.79 + atmosphere * .711)))
             : -20.774 / te) / 3600 * radians;
@@ -43,10 +52,10 @@
     }
 
     function noon(date) {
-      var centuries = (d3.time.day.utc.floor(date) - J2000) / (864e5 * 36525),
+      var centuries = (d3.utcDay.floor(date) - J2000) / (864e5 * 36525),
           minutes = (minutesOffset - (equationOfTime(centuries + (minutesOffset - (equationOfTime(centuries - longitude / (360 * 365.25 * 100)) * degrees * 4)) / (1440 * 365.25 * 100)) * degrees * 4) - date.getTimezoneOffset()) % 1440;
       if (minutes < 0) minutes += 1440;
-      return new Date(+d3.time.day.floor(date) + minutes * 60 * 1000);
+      return new Date(+d3.timeDay.floor(date) + minutes * 60 * 1000);
     }
 
     return {
@@ -59,21 +68,21 @@
     var e = eccentricityEarthOrbit(centuries),
         m = solarGeometricMeanAnomaly(centuries),
         l = solarGeometricMeanLongitude(centuries),
-        y = Math.tan(obliquityCorrection(centuries) / 2);
+        y = tan(obliquityCorrection(centuries) / 2);
     y *= y;
-    return y * Math.sin(2 * l)
-        - 2 * e * Math.sin(m)
-        + 4 * e * y * Math.sin(m) * Math.cos(2 * l)
-        - 0.5 * y * y * Math.sin(4 * l)
-        - 1.25 * e * e * Math.sin(2 * m);
+    return y * sin(2 * l)
+        - 2 * e * sin(m)
+        + 4 * e * y * sin(m) * cos(2 * l)
+        - 0.5 * y * y * sin(4 * l)
+        - 1.25 * e * e * sin(2 * m);
   }
 
   function solarDeclination(centuries) {
-    return Math.asin(Math.sin(obliquityCorrection(centuries)) * Math.sin(solarApparentLongitude(centuries)));
+    return asin(sin(obliquityCorrection(centuries)) * sin(solarApparentLongitude(centuries)));
   }
 
   function solarApparentLongitude(centuries) {
-    return solarTrueLongitude(centuries) - (0.00569 + 0.00478 * Math.sin((125.04 - 1934.136 * centuries) * radians)) * radians;
+    return solarTrueLongitude(centuries) - (0.00569 + 0.00478 * sin((125.04 - 1934.136 * centuries) * radians)) * radians;
   }
 
   function solarTrueLongitude(centuries) {
@@ -86,18 +95,18 @@
 
   function solarGeometricMeanLongitude(centuries) {
     var l = (280.46646 + centuries * (36000.76983 + centuries * 0.0003032)) % 360;
-    return (l < 0 ? l + 360 : l) / 180 * π;
+    return (l < 0 ? l + 360 : l) / 180 * pi;
   }
 
   function solarEquationOfCenter(centuries) {
     var m = solarGeometricMeanAnomaly(centuries);
-    return (Math.sin(m) * (1.914602 - centuries * (0.004817 + 0.000014 * centuries))
-        + Math.sin(m + m) * (0.019993 - 0.000101 * centuries)
-        + Math.sin(m + m + m) * 0.000289) * radians;
+    return (sin(m) * (1.914602 - centuries * (0.004817 + 0.000014 * centuries))
+        + sin(m + m) * (0.019993 - 0.000101 * centuries)
+        + sin(m + m + m) * 0.000289) * radians;
   }
 
   function obliquityCorrection(centuries) {
-    return meanObliquityOfEcliptic(centuries) + 0.00256 * Math.cos((125.04 - 1934.136 * centuries) * radians) * radians;
+    return meanObliquityOfEcliptic(centuries) + 0.00256 * cos((125.04 - 1934.136 * centuries) * radians) * radians;
   }
 
   function meanObliquityOfEcliptic(centuries) {
