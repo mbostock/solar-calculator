ed27fae8ed65f6beae3c445bdb9f284c994d6ef4
Mike Bostock <mbostock@gmail.com>
2017-06-09 11:12:08 -0700

    Simplify slightly.

--- src/noon.js
+++ src/noon.js
@@ -3,10 +3,8 @@ import day from "./day";
 import equationOfTime from "./equationOfTime";
 
 export default function(date, longitude) {
-  var t = century(+day(date) + (12 - longitude * 24 / 360) * 36e5), // Guess local noon.
-      t0 = t - longitude / (360 * 36525),
-      offset = 720 - longitude * 4 - equationOfTime(t0), // minutes
-      t1 = t + offset / (1440 * 36525),
-      local = 720 - longitude * 4 - equationOfTime(t1); // minutes
-  return new Date(+day(date) + local * 1000 * 60);
+  var t = century(+day(date) + (12 - longitude * 24 / 360) * 36e5), // First approximation.
+      o1 = 720 - longitude * 4 - equationOfTime(t - longitude / (360 * 36525)), // First correction.
+      o2 = 720 - longitude * 4 - equationOfTime(t + o1 / (1440 * 36525)); // Second correction.
+  return new Date(+day(date) + o2 * 1000 * 60);
 }
