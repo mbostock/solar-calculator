5c0597918622bba7accd328d790edcc6bd9460b2
Mike Bostock <mbostock@gmail.com>
2017-06-09 10:47:13 -0700

    Fix test.

--- test/declination-test.js
+++ test/declination-test.js
@@ -1,7 +1,7 @@
 var tape = require("tape"),
     solar = require("../");
 
-tape("declination(2017-06-09T09:42:42-0700) is 22.98 degrees", function(test) {
+tape("declination(2017-06-09T09:42:42-0700) is 22.98°", function(test) {
   test.equal(Math.round(solar.declination(solar.century(new Date("2017-06-09T09:42:42-0700"))) * 100) / 100, 22.98);
   test.end();
 });
--- test/riseHourAngle-test.js
+++ test/riseHourAngle-test.js
@@ -1,8 +1,7 @@
-var d3 = require("d3-time"),
-    tape = require("tape"),
+var tape = require("tape"),
     solar = require("../");
 
-tape("riseHourAngle is negative for sunrise", function(test) {
-  test.equal(+d3.utcSecond(solar.noon(new Date("2017-06-09T09:42:42-0700"), -122.4194)), +new Date("2017-06-09T13:09:12-0700"));
+tape("riseHourAngle(2017-06-09T09:42:42-0700, 37.7749°N) is -110.40°", function(test) {
+  test.equal(Math.round(solar.riseHourAngle(new Date("2017-06-09T09:42:42-0700"), 37.7749) * 100)  / 100, -110.40);
   test.end();
 });
